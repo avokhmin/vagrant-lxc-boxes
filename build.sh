6bf7bb68ae398fe311c4f33227c8793a47bb1e3f
Vokhmin Alexey V <avokhmin@gmail.com>
2013-10-07 22:38:22 +0400

    rename of variable

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 PRODUCTNAME=${PRODUCTNAME:-"vagrant-box"}
-productname_type="$PRODUCTNAME_TYPE"
+product_type="$PRODUCT_TYPE"
 
 BRANCH=${BRANCH:-"v0.6.1-abf-worker-service"}
 
 script_path=`pwd`
 
-PRODUCTNAME=$PRODUCTNAME BRANCH=$BRANCH /bin/bash $script_path/$productname_type.sh
+PRODUCTNAME=$PRODUCTNAME BRANCH=$BRANCH /bin/bash "$script_path/$product_type.sh"
