f0b864cb47739c3a83455d10f85529dd77d404eb
Vokhmin Alexey V <avokhmin@gmail.com>
2013-10-10 18:46:04 +0400

    remove unnecessary code

--- mdv.sh
+++ mdv.sh
@@ -2,15 +2,13 @@
 
 ch="$PRODUCTNAME"
 echo $PRODUCTNAME
-echo $REPO
 echo $ARCH
 mkdir $ch
-umount $ch/sys
-umount $ch/proc
-umount $ch/dev/pts
-umount $ch/dev
-urpmi.addmedia --urpmi-root $PRODUCTNAME --distrib --all-media $REPO > /home/vagrant/results/addrepo.log 2>&1
+
+repo=http://abf-downloads.rosalinux.ru/$PRODUCTNAME/repository/$ARCH
+urpmi.addmedia --urpmi-root $PRODUCTNAME --distrib --all-media $repo > /home/vagrant/results/addrepo.log 2>&1
 urpmi --urpmi-root $PRODUCTNAME --root $PRODUCTNAME basesystem-minimal basesystem urpmi rpm locales-en locales-ru rpm-build syslinux yum bootloader-utils git lxc --auto --no-suggests --no-verify-rpm
+
 mkdir -p $ch/dev
 mkdir -p $ch/dev/pts
 mkdir -p $ch/proc
@@ -25,43 +23,10 @@ cp /etc/resolv.conf $ch/etc/
 
 
 chroot $ch git clone -b $BRANCH https://github.com/avokhmin/vagrant-lxc.git $ch/opt/VAGRANT-LXC-BOX-BUILD
-cd $ch/$ch/opt/VAGRANT-LXC-BOX-BUILD
-cd ../../../../
 ls -R > /home/vagrant/results/ls.log
-
-
-
 
 echo 
 echo "----------> UR IN Z MATRIX <----------"
-
-# Configure network
-# sudo chkconfig NetworkManager off
-# sudo chkconfig --level 35 network on
-# sudo service NetworkManager stop
-# sudo service network restart
-
-# sudo cat <<"EOF"> /etc/sysconfig/network-scripts/ifcfg-eth0
-# DEVICE=eth0
-# BOOTPROTO=none
-# BRIDGE=br0
-# ONBOOT=yes
-# EOF
-
-# sudo cat <<"EOF"> /etc/sysconfig/network-scripts/ifcfg-br0 
-# DEVICE=br0
-# BOOTPROTO=dhcp
-# ONBOOT=yes
-# TYPE=Bridge
-# DELAY=0
-# EOF
-
-
-# sudo cat <<"EOF"> /etc/sysconfig/network
-# NETWORKING=yes
-# EOF
-
-# sudo service network restart
 
 /usr/sbin/chroot $ch ip a
 
@@ -72,3 +37,5 @@ umount -l $ch/proc
 umount -l $ch/dev/pts
 umount -l $ch/iso
 umount -l $ch/dev
+
+rm -rf $ch
